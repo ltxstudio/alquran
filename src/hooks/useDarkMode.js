e8108cf1daddfb9404e5b150cf8c5128483d918b
ltxstudio <ltxstudio.info@gmail.com>
2024-11-16 05:27:23 +0600

    Create useDarkMode.js

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react';
+
+export const useDarkMode = () => {
+  const [darkMode, setDarkMode] = useState(() => localStorage.getItem('darkMode') === 'true');
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+    localStorage.setItem('darkMode', darkMode);
+  }, [darkMode]);
+
+  const toggleDarkMode = () => setDarkMode(!darkMode);
+
+  return [darkMode, toggleDarkMode];
+};
